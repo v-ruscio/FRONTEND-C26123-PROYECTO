b3dd81e596ffb07bfae55cbdd21ca1a0e7882199
Victoria <victoriaruscio@gmail.com>
2026-07-05 00:12:26 -0300

    incorporacion de sweetalert

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,22 @@
 export const actualizarContador = (carrito) => {
-    const contador = document.getElementById("contador-carrito");
-    if(contador){
-        contador.textContent = carrito.length;
-    }
+  const contador = document.getElementById("contador-carrito");
+  if (contador) {
+    contador.textContent = carrito.length;
+  }
 };
 
-
 export const mostrarMensaje = (mensaje) => {
-    alert(mensaje);
+  Swal.fire({
+    title: mensaje,
+    icon: "success",
+    draggable: true,
+    confirmButtonText: "Genial!",
+    backdrop: false,
+    customClass: {
+      confirmButton: "btn-msj",
+      popup: "box-msj",
+    },
+  });
+
+  /*   alert(mensaje); */
 };
